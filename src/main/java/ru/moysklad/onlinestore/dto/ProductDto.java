fafa859d6b2bb533e4555e2a5147488834bd7b61
nikitaArbuzArbuz <talker_64@vk.com>
2024-11-26 00:08:44 +0300

    t4: add - filter and sort for products list

--- src/main/java/ru/moysklad/onlinestore/dto/ProductDto.java
+++ src/main/java/ru/moysklad/onlinestore/dto/ProductDto.java
@@ -3,7 +3,6 @@ package ru.moysklad.onlinestore.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
